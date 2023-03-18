66fc6829ddf0d0ef55618ebbe7067a0b4575a5c0
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-03-18 17:18:49 +0000

    Ex. 4.1.3: missing PKs, capital letters

--- ex_4_1_3.sql
+++ ex_4_1_3.sql
@@ -1,43 +1,44 @@
-DROP TABLE IF EXISTS order_management.rel_prod_enc;
-DROP TABLE IF EXISTS order_management.rel_enc_forn;
-DROP TABLE IF EXISTS order_management.encomenda;
-DROP TABLE IF EXISTS order_management.fornecedor;
-DROP TABLE IF EXISTS order_management.produto;
-DROP TABLE IF EXISTS order_management.empresa;
-DROP SCHEMA IF EXISTS order_management;
+DROP TABLE IF EXISTS ORDER_MANAGEMENT.REL_PROD_ENC;
+DROP TABLE IF EXISTS ORDER_MANAGEMENT.REL_ENC_FORN;
+DROP TABLE IF EXISTS ORDER_MANAGEMENT.ENCOMENDA;
+DROP TABLE IF EXISTS ORDER_MANAGEMENT.FORNECEDOR;
+DROP TABLE IF EXISTS ORDER_MANAGEMENT.PRODUTO;
+DROP TABLE IF EXISTS ORDER_MANAGEMENT.EMPRESA;
+DROP SCHEMA IF EXISTS ORDER_MANAGEMENT;
 GO
 
-CREATE SCHEMA order_management;
+CREATE SCHEMA ORDER_MANAGEMENT;
 GO
 
-CREATE TABLE order_management.empresa (
+CREATE TABLE ORDER_MANAGEMENT.EMPRESA (
     nif                     INT             NOT NULL    PRIMARY KEY
     CHECK (nif > 100000000)
 )
 
-CREATE TABLE order_management.produto (
+CREATE TABLE ORDER_MANAGEMENT.PRODUTO (
     codigo                  INT             NOT NULL    PRIMARY KEY,
     preco                   INT             NOT NULL,
     taxa_de_iva             INT             NOT NULL    DEFAULT 23,
     unidades_armazem        INT             NOT NULL    DEFAULT 0,
     nif                     INT             NOT NULL,
-    FOREIGN KEY (nif) REFERENCES order_management.empresa(nif) ON UPDATE CASCADE,
+    FOREIGN KEY (nif) REFERENCES ORDER_MANAGEMENT.EMPRESA(nif) ON UPDATE CASCADE,
     CHECK (preco >= 0), -- It can be a gift, hence why it can cost 0€
     CHECK (taxa_de_iva >= 0),
 )
 
-CREATE TABLE order_management.encomenda (
+CREATE TABLE ORDER_MANAGEMENT.ENCOMENDA (
     numero                  INT             NOT NULL    PRIMARY KEY,
     data_encomenda          DATE            NOT NULL,
     nif                     INT             NOT NULL
 )
 
-CREATE TABLE order_management.rel_prod_enc (
-    codigo_produto          INT             NOT NULL    REFERENCES order_management.produto(codigo) ON UPDATE CASCADE,
-    numero_encomenda        INT             NOT NULL    REFERENCES order_management.encomenda(numero) ON UPDATE CASCADE
+CREATE TABLE ORDER_MANAGEMENT.REL_PROD_ENC (
+    codigo_produto          INT             NOT NULL    REFERENCES ORDER_MANAGEMENT.PRODUTO(codigo) ON UPDATE CASCADE,
+    numero_encomenda        INT             NOT NULL    REFERENCES ORDER_MANAGEMENT.ENCOMENDA(numero) ON UPDATE CASCADE,
+    PRIMARY KEY (codigo_produto, numero_encomenda)
 )
 
-CREATE TABLE order_management.fornecedor (
+CREATE TABLE ORDER_MANAGEMENT.FORNECEDOR (
     nif                     INT             NOT NULL    PRIMARY KEY,
     nome                    VARCHAR(100),
     fax                     INT,
@@ -47,9 +48,10 @@ CREATE TABLE order_management.fornecedor (
     CHECK (nif > 100000000)
 )
 
-CREATE TABLE order_management.rel_enc_forn (
-    numero_encomenda        INT             NOT NULL    REFERENCES order_management.encomenda(numero) ON UPDATE CASCADE,
-    nif_fornecedor          INT             NOT NULL    REFERENCES order_management.fornecedor(nif) ON UPDATE CASCADE
+CREATE TABLE ORDER_MANAGEMENT.REL_ENC_FORN (
+    numero_encomenda        INT             NOT NULL    REFERENCES ORDER_MANAGEMENT.ENCOMENDA(numero) ON UPDATE CASCADE,
+    nif_fornecedor          INT             NOT NULL    REFERENCES ORDER_MANAGEMENT.FORNECEDOR(nif) ON UPDATE CASCADE,
+    PRIMARY KEY (numero_encomenda, nif_fornecedor)
 )
 
-ALTER TABLE order_management.encomenda ADD CONSTRAINT nif FOREIGN KEY (nif) REFERENCES order_management.fornecedor(nif)
+ALTER TABLE ORDER_MANAGEMENT.ENCOMENDA ADD CONSTRAINT nif FOREIGN KEY (nif) REFERENCES ORDER_MANAGEMENT.FORNECEDOR(nif)
