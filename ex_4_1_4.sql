53b75832fc982255ea6f77741493413763be2eb4
zegameiro <jose.mcgameiro@ua.pt>
2023-03-18 17:26:46 +0000

    Finished ex_4_1_4

--- ex_4_1_4.sql
+++ ex_4_1_4.sql
@@ -1,87 +1,89 @@
 -- Exercício 4.1.4
 
-DROP TABLE IF EXISTS pharmacy_management.rel_drug_pharmacy;
-DROP TABLE IF EXISTS pharmacy_management.pharmacy;
-DROP TABLE IF EXISTS pharmacy_management.rel_drug_pharmaceutical;
-DROP TABLE IF EXISTS pharmacy_management.pharmaceutical;
-DROP TABLE IF EXISTS pharmacy_management.rel_presc_drug;
-DROP TABLE IF EXISTS pharmacy_management.drug;
-DROP TABLE IF EXISTS pharmacy_management.prescription;
-DROP TABLE IF EXISTS pharmacy_management.user;
-DROP TABLE IF EXISTS pharmacy_management.doctor;
-DROP SCHEMA IF EXISTS pharmacy_management;
+DROP TABLE IF EXISTS PHARMACY_MANAGEMENT.REL_DRUG_PHARMACY;
+DROP TABLE IF EXISTS PHARMACY_MANAGEMENT.REL_DRUG_PHARMACEUTICAL;
+DROP TABLE IF EXISTS PHARMACY_MANAGEMENT.PHARMACEUTICAL;
+DROP TABLE IF EXISTS PHARMACY_MANAGEMENT.REL_PRESCRIPTION_DRUG;
+DROP TABLE IF EXISTS PHARMACY_MANAGEMENT.DRUG;
+DROP TABLE IF EXISTS PHARMACY_MANAGEMENT.PRESCRIPTION;
+DROP TABLE IF EXISTS PHARMACY_MANAGEMENT.PHARMACY;
+DROP TABLE IF EXISTS PHARMACY_MANAGEMENT.PATIENT;
+DROP TABLE IF EXISTS PHARMACY_MANAGEMENT.DOCTOR;
+DROP SCHEMA IF EXISTS PHARMACY_MANAGEMENT;
 GO
 
-CREATE SCHEMA pharmacy_management;
+CREATE SCHEMA PHARMACY_MANAGEMENT;
 GO
 
-CREATE TABLE pharmacy_management.doctor (
-    number          INT             NOT NULL         PRIMARY KEY,
-    name            VARCHAR(30)     NOT NULL         UNIQUE,
-    specialty       VARCHAR(100)    NOT NULL,
+CREATE TABLE PHARMACY_MANAGEMENT.DOCTOR (
+    number              INT             NOT NULL        PRIMARY KEY,
+    name                VARCHAR(30)     NOT NULL,
+    specialty           VARCHAR(100)    NOT NULL,
 );
 
-CREATE TABLE pharmacy_management.user (
-    number          INT             NOT NULL        PRIMARY KEY,
-    name            VARCHAR(30)     NOT NULL        UNIQUE,
-    birth_date      DATE            NOT NULL,
-    address         VARCHAR(100)    NOT NULL,
+CREATE TABLE PHARMACY_MANAGEMENT.PATIENT (
+    number              INT             NOT NULL        PRIMARY KEY,
+    name                VARCHAR(30)     NOT NULL,
+    birth_date          DATE            NOT NULL,
+    address             VARCHAR(50)     NOT NULL,
 );
 
-CREATE TABLE pharmacy_management.prescription (
-    number          INT             NOT NULL         PRIMARY KEY,
-    creation_date   DATE            NOT NULL,
-    dispatch_date   DATE            NOT NULL,
-    doctor_number   INT             NOT NULL,
-    user_number     INT             NOT NULL,
-    user_nif        INT             NOT NULL,
-
-    FOREIGN KEY (doctor_number) REFERENCES pharmacy_management.doctor(number),
-    FOREIGN KEY (user_number) REFERENCES pharmacy_management.user(number),
-    FOREIGN KEY (user_nif) REFERENCES pharmacy_management.user(nif)
+CREATE TABLE PHARMACY_MANAGEMENT.PHARMACY (
+    nif                 INT             NOT NULL        PRIMARY KEY,
+    name                VARCHAR(30)     NOT NULL,
+    phone_number        INT             NOT NULL,
+    address             VARCHAR(50)     NOT NULL,
 );
 
-CREATE TABLE pharmacy_management.drug (
-    formula         VARCHAR(100)    NOT NULL        PRIMARY KEY,
+CREATE TABLE PHARMACY_MANAGEMENT.PRESCRIPTION (
+    number              INT             NOT NULL        PRIMARY KEY,
+    creation_date       DATE            NOT NULL,
+    dispatch_date       DATE            NOT NULL,
+    DOCTOR_number       INT             NOT NULL,
+    PATIENT_number      INT             NOT NULL,
+    PHARMACY_nif        INT             NOT NULL,
+
+    FOREIGN KEY (DOCTOR_number) REFERENCES PHARMACY_MANAGEMENT.DOCTOR(number),
+    FOREIGN KEY (PATIENT_number) REFERENCES PHARMACY_MANAGEMENT.PATIENT(number),
+    FOREIGN KEY (PHARMACY_nif) REFERENCES PHARMACY_MANAGEMENT.PHARMACY(nif),
+    CHECK (creation_date < dispatch_date),
 );
 
-CREATE TABLE pharmacy_management.rel_presc_drug (
-    presc_number    INT             NOT NULL,
-    drug_formula    VARCHAR(100)    NOT NULL,
-
-    PRIMARY KEY (presc_number, drug_formula),
-    FOREIGN KEY (presc_number) REFERENCES pharmacy_management.prescription(number),
-    FOREIGN KEY (drug_formula) REFERENCES pharmacy_management.drug(formula)
+CREATE TABLE PHARMACY_MANAGEMENT.DRUG (
+    formula             VARCHAR(100)    NOT NULL        PRIMARY KEY,
 );
 
-CREATE TABLE pharmacy_management.pharmaceutical (
-    register_number INT             NOT NULL        PRIMARY KEY,
-    name            VARCHAR(30)     NOT NULL        UNIQUE,
-    phone_number    INT             NOT NULL        UNIQUE,
-    address         VARCHAR(100)    NOT NULL,
+CREATE TABLE PHARMACY_MANAGEMENT.REL_PRESCRIPTION_DRUG (
+    PRESCRIPTION_number INT             NOT NULL,
+    DRUG_formula        VARCHAR(100)    NOT NULL,
+
+    PRIMARY KEY (PRESCRIPTION_number, DRUG_formula),
+    FOREIGN KEY (PRESCRIPTION_number) REFERENCES PHARMACY_MANAGEMENT.PRESCRIPTION(number),
+    FOREIGN KEY (DRUG_formula) REFERENCES PHARMACY_MANAGEMENT.DRUG(formula),
 );
 
-CREATE TABLE pharmacy_management.rel_drug_pharmaceutical (
-    drug_formula    VARCHAR(100)    NOT NULL,
-    register_number INT             NOT NULL,
-
-    PRIMARY KEY (drug_formula, register_number),
-    FOREIGN KEY (drug_formula) REFERENCES pharmacy_management.drug(formula),
-    FOREIGN KEY (register_number) REFERENCES pharmacy_management.pharmaceutical(register_number),
+CREATE TABLE PHARMACY_MANAGEMENT.PHARMACEUTICAL (
+    register_number     INT             NOT NULL        PRIMARY KEY,
+    name                VARCHAR(30)     NOT NULL,
+    phone_number        INT             NOT NULL,
+    address             VARCHAR(50)     NOT NULL,
 );
 
-CREATE TABLE pharmacy_management.pharmacy (
-    nif             INT             NOT NULL        PRIMARY KEY,
-    name            VARCHAR(30)     NOT NULL        UNIQUE,
-    phone_number    INT             NOT NULL        UNIQUE,
-    address         VARCHAR(100)    NOT NULL,
+CREATE TABLE PHARMACY_MANAGEMENT.REL_DRUG_PHARMACEUTICAL (
+    PHAR_reg_number     INT             NOT NULL,
+    DRUG_formula        VARCHAR(100)    NOT NULL,
+
+    PRIMARY KEY (PHAR_reg_number, DRUG_formula),
+    FOREIGN KEY (PHAR_reg_number) REFERENCES PHARMACY_MANAGEMENT.PHARMACEUTICAL(register_number),
+    FOREIGN KEY (DRUG_formula) REFERENCES PHARMACY_MANAGEMENT.DRUG(formula),
 );
 
-CREATE TABLE pharmacy_management.rel_drug_pharmacy (
-    drug_formula    VARCHAR(100)    NOT NULL,
-    pharmacy_nif    INT             NOT NULL,
+CREATE TABLE PHARMACY_MANAGEMENT.REL_DRUG_PHARMACY (
+    DRUG_formula        VARCHAR(100)    NOT NULL,
+    PHARMACY_nif        INT             NOT NULL,
 
-    PRIMARY KEY (drug_formula, pharmacy_nif),
-    FOREIGN KEY (drug_formula) REFERENCES pharmacy_management.drug(drug_formula),
-    FOREIGN KEY (pharmacy_nif) REFERENCES pharmacy_management.pharmacy(nif)
+    PRIMARY KEY (DRUG_formula, PHARMACY_nif),
+    FOREIGN KEY (DRUG_formula) REFERENCES PHARMACY_MANAGEMENT.DRUG(formula),
+    FOREIGN KEY (PHARMACY_nif) REFERENCES PHARMACY_MANAGEMENT.PHARMACY(nif),
 );
+
